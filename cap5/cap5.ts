b1031baf840e400fd9ac12e66204d5347452a755
fabocv <fabianleandro.cv@gmail.com>
2025-09-17 20:02:58 -0300

    cap5

---
+++ cap5/cap5.ts
@@ -0,0 +1,28 @@
+class ConfigManager {
+  private static instance: ConfigManager;
+  private config: Record<string, any> = {};
+
+  private constructor() {} // 👈 evita instanciación directa
+
+  public static getInstance(): ConfigManager {
+    if (!ConfigManager.instance) {
+      ConfigManager.instance = new ConfigManager();
+    }
+    return ConfigManager.instance;
+  }
+
+  public set(key: string, value: any) {
+    this.config[key] = value;
+  }
+
+  public get(key: string) {
+    return this.config[key];
+  }
+}
+
+// Uso
+const config1 = ConfigManager.getInstance();
+config1.set("api", "google.com");
+
+const config2 = ConfigManager.getInstance();
+console.log(config1 === config2); // true 
